e53fb26b7f0503df16b9f77eb9bd021ccd13f387
Vlad2346543 <114310954+Vlad2346543@users.noreply.github.com>
2026-02-10 05:08:31 +0200

    install commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import type { Movie } from "./types/movie";
-import { fetchMovies } from "./components/services/movieService";
+import { fetchMovies } from "./services/movieService";
 
 import SearchBar from "./components/SearchBar/SearchBar";
 import MovieGrid from "./components/MovieGrid/MovieGrid";
